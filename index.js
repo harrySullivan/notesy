17c9794813de0e3f728d91622a0cbc310262b57a
Harry Sullivan <aditya.michigan@gmail.com>
2020-05-06 13:36:06 -0400

    remvoe autogrow

--- index.js
+++ index.js
@@ -104,20 +104,20 @@ function insertAtCursor(input, textToInsert) {
 
 var autoExpand = function (field) {
 
-  // Reset field height
-  field.style.height = 'inherit';
+  // // Reset field height
+  // field.style.height = 'inherit';
 
-  // Get the computed styles for the element
-  var computed = window.getComputedStyle(field);
+  // // Get the computed styles for the element
+  // var computed = window.getComputedStyle(field);
 
-  // Calculate the height
-  var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
-    + parseInt(computed.getPropertyValue('padding-top'), 10)
-    + field.scrollHeight
-    + parseInt(computed.getPropertyValue('padding-bottom'), 10)
-    + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+  // // Calculate the height
+  // var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+  //   + parseInt(computed.getPropertyValue('padding-top'), 10)
+  //   + field.scrollHeight
+  //   + parseInt(computed.getPropertyValue('padding-bottom'), 10)
+  //   + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
 
-  field.style.height = height + 'px';
+  // field.style.height = height + 'px';
 
 };
 
